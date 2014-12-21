68c17211a5967b287e6e8ff812022b93b1790c53
sergeyt <stodyshev@gmail.com>
2014-12-21 19:06:25 +0600

    diagnostic message

--- test/PersonSpec.ts
+++ test/PersonSpec.ts
@@ -4,6 +4,7 @@
 describe("Person test", () => {
 
   it("should have a name", () => {
+    console.log("I expect to see this message in console.");
     var person = new unitTestExample.Person("Joe");
     expect(person.name).toBe("Joe");
   });
